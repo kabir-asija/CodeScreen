7a215d12844e634a495f8d84f455c4136ecf87c9
Kabir Asija <kabirasija3@gmail.com>
2025-12-03 15:29:18 +0530

    deployment config added

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,14 +1,23 @@
-import express from 'express';
-import dotenv from 'dotenv';
+import express from "express";
+import path from "path";
+import dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+const __dirname = path.resolve();
 
 app.get("/", (req, res) => {
-    res.send("Hello, World!");
+  res.send("Hello, World!");
 });
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.get("/{*any}", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+  });
+}
+
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
