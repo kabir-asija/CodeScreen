0ccb9d61e0ba714c410316a49944dee2b7526106
Kabir Asija <kabirasija3@gmail.com>
2025-12-04 16:06:03 +0530

    auth and db sync

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,15 +2,25 @@ import express from "express";
 import path from "path";
 import dotenv from "dotenv";
 import { connectDb } from "./lib/db.js";
+import cors from 'cors'
+import {serve } from 'inngest/express'
+import {inngest} from './lib/inngest.js'
+
+
 dotenv.config({quiet: true});
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 const __dirname = path.resolve();
 
-app.get("/", (req, res) => {
+app.use(express.json())
+app.use(cors({origin:process.env.CLIENT_URL, credentials:true}))
+
+app.get("/hello", (req, res) => {
   res.send("Hello, World!");
 });
+
+app.get("/api/inngest", serve({client:inngest, functions}))
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
